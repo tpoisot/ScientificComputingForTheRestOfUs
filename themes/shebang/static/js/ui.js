b118ec5b831ce28a94992757232ad49d073f6166
Timothée Poisot <timothee.poisot@umontreal.ca>
2019-05-07 21:17:04 -0400

    collapsable sidebar

--- themes/shebang/static/js/ui.js
+++ themes/shebang/static/js/ui.js
@@ -1,3 +1,7 @@
+document.querySelector(".button").addEventListener('click', () => {
+  document.querySelector(".content").classList.toggle('isOpen');
+});
+
 function toggle_status() {
     var div = document.getElementById('status');
     div.style.display = div.style.display == 'none' ? 'block' : 'none';

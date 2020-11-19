3b3df775e729b68a83957ec40260b2265c0538a0
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-19 12:17:25 -0500

    up

--- _builder.jl
+++ _builder.jl
@@ -4,12 +4,10 @@ lesson = first(ARGS)
 
 import Pkg
 using Weave
-root = pwd()
-target_file = joinpath("dist", replace(source_file, "index.Jmd" => "index.md"))
 cd(joinpath("content", lesson))
 Pkg.activate(".")
 weave(
     "_index.Jmd",
-    out_path=joinpath(pwd(), "dist", "content", lesson, "index.md"),
+    out_path=joinpath("..", "..", "..", "dist", "content", lesson, "_index.md"),
     doctype="github"
 )

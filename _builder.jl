bf423c973263fefdbf85050cac271a569a3ef268
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-19 13:03:04 -0500

    update lessons builder

--- _builder.jl
+++ _builder.jl
@@ -3,11 +3,29 @@
 lesson = first(ARGS)
 
 import Pkg
+
 using Weave
+using TOML
+using JSON
+
 cd(joinpath("content", lesson))
+
 Pkg.activate(".")
+
+if isfile("Project.toml")
+    _proj = TOML.parsefile("Project.toml")
+    @info collect(keys(_proj["deps"]))
+    dpath = joinpath("..","..", "..", "dist", "data", "dependencies", lesson)
+    ispath(dpath) || mkpath(dpath)
+    open(joinpath(dpath, "deps.json"),"w") do f
+        JSON.print(f, _proj)
+    end
+end
+
+#=
 weave(
     "_index.Jmd",
     out_path=joinpath("..", "..", "..", "dist", "content", lesson, "_index.md"),
     doctype="github"
 )
+=#

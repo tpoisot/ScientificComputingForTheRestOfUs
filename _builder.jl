57bae09dce112e616b375e78e5c35e51656379e5
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-11-19 11:56:50 -0500

    julia weaving script update

--- _builder.jl
+++ _builder.jl
@@ -1,2 +1,15 @@
 @info ARGS
-lesson
+
+lesson = first(ARGS)
+
+import Pkg
+using Weave
+source_file = joinpath("content", lesson, "_index.Jmd")
+target_file = joinpath("dist", replace(source_file, "index.Jmd" => "index.md"))
+cd(joinpath("content", lesson))
+Pkg.activate(".")
+weave(
+    source_file,
+    out_path=target_file,
+    doctype="github"
+)

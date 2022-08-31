a7ac1bc418c159a34a0685fc55090ea2bb17c640
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-08-30 20:58:39 -0400

    logo update

--- logomaker/logo.jl
+++ logomaker/logo.jl
@@ -1,9 +1,10 @@
 using Luxor
 
 using Random
-Random.seed!(123)
+Random.seed!(420)
 
 # theme colors
+c0 = (66, 148, 150) ./ 255;
 c1 = (255, 138, 67) ./ 255;
 c2 = (112, 110, 177) ./ 255;
 c3 = (0, 131, 166) ./ 255;
@@ -12,36 +13,26 @@ Drawing(500, 500, joinpath(@__DIR__, "..", "dist", "static", "logo.svg"))
 
 origin()
 
-setcolor(66 / 255, 148 / 255, 150 / 255)
-rotate(deg2rad(15))
-#squircle(O, 200, 200; rt = 0.5, action = :stroke)
-rotate(deg2rad(-3))
+setline(1)
+setcolor(c0)
 
-for i in 1:12
-    #setcolor(rand([c1, c2, c3])...)
-    #setcolor(c2...)
-    rotate(i * deg2rad(41))
-    corners = ngon(Point(0, 0), 20 + 12i, 6; vertices = true)
-    circle.(corners, (12 - i + 1), action = rand() < 0.5 ? :fill : :stroke)
+for i in 1:8
+    circle(rand(-15:15), rand(-15:15), rand(200:240), action = :stroke)
 end
 
-finish()
-preview()
+setline(3)
+setcolor(c2)
+circle(O, 220, action = :stroke)
 
-Drawing(500, 500, joinpath(@__DIR__, "..", "dist", "static", "favicon.svg"))
-
-origin()
-
-setcolor(66 / 255, 148 / 255, 150 / 255)
-rotate(deg2rad(15))
-squircle(O, 200, 200; rt = 0.5, action = :fill)
-rotate(deg2rad(-3))
-
-for i in 1:12
-    setcolor(255, 255, 255)
-    rotate(i * deg2rad(41))
-    corners = ngon(Point(0, 0), 20 + 12i, 6; vertices = true)
-    circle.(corners, (12 - i + 1), action = rand() < 0.5 ? :fill : :stroke)
+for i in 1:10
+    rotate(deg2rad(3i))
+    corners = ngon(Point(0, 0), 20 + 15i, 5; vertices = true)
+    for corner in corners
+        rotate(deg2rad(rand(-5:5)))
+        setline(rand(2:5))
+        setcolor(rand([c1, c3]))
+        circle(corner, rand(2:6); action = rand([:fill, :stroke]))
+    end
 end
 
 finish()

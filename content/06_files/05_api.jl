01194fae47c8f53f7444c08da12eedf44d6a2a44
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-05 11:22:03 -0400

    🧑‍🎓 API done

--- content/06_files/05_api.jl
+++ content/06_files/05_api.jl
@@ -1,11 +1,24 @@
 # ---
 # title: Consuming data from APIs
-# status: alpha
+# status: beta
 # ---
+
+# Not all data come from static files. In a large number of scientific
+# applications, we need to collect data from websites, often using a [RESTful
+# API][rest]. In this module, we will use a very simple example to show how we
+# can collect data about postal codes, get them as a JSON file, and process
+# them.
+
+# [rest]: https://www.redhat.com/en/topics/api/what-is-a-rest-api
 
 # <!--more-->
 
-using HTTP, JSON
+# In order to make this example work, we will need two things: a way to interact
+# with remote servers (provided by the *HTTP* package), and a way to read JSON
+# data (using *JSON*, as we have seen in the previous module.)
+
+import HTTP
+import JSON
 
 # In this example, we will get the names and coordinates of the various post
 # codes in [Rimouski][riki], a (small) city in Québec, Canada. The
@@ -30,16 +43,90 @@ using HTTP, JSON
 
 # [iana]: https://www.iana.org/assignments/http-status-codes/http-status-codes.xhtml
 
-endpoint = "https://api.zippopotam.us/ca/qc/rimouski"
+# !!!INFO HTTP status codes are a "soft" standard that most people and services
+# agree on. For example, `404` means that the resource was not found, `403` that
+# it is forbidden to access, and `500` that something went wrong on the server.
+# If you work with APIs often as part of your work, it is very important to get
+# acquainted with them.
 
-#-
+# In our case, after reading the API documentation, we know that we need to get
+# to the endpoint that is given by `country/province/name`:
+
+api_root = "https://api.zippopotam.us"
+place = (country = "ca", province = "qc", name = "rimouski")
+endpoint = "$(api_root)/$(place.country)/$(place.province)/$(place.name)"
+
+# Now that we have this endpoint setup, we can perform a `GET` request, which is
+# one of the many [HTTP verbs][verbs]. A `GET` request will request the content
+# of a page for our consumption. Other commnly used verbs include `LIST` (to get
+# a list of multiple resources), `POST` to upload formatted data, and `PATCH` to
+# edit data on the API.
+
+# [verbs]: https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
 
 res = HTTP.get(endpoint)
 
+# Because `res` is a new type of object, we can take a look at its fields:
+
+typeof(res)
+
 #-
 
-res.status
+fieldnames(typeof(res))
 
-#- 
+# The first thing we want to check is the response status, specifically that it
+# is equal to `200`:
 
-JSON.parse(String(res.body))
+isequal(200)(res.status)
+
+# We can also inspect the headers:
+
+res.headers
+
+# This stores a lot of interesting information, but as a vector of pairs. We can
+# make our life significantly easier by turning this into a dictionary. For
+# example, we can confirm that the API is indeed giving us data in the
+# `application/json` format:
+
+Dict(res.headers)["Content-Type"]
+
+# With this information, we can get the actual content of our request, which is
+# stored in the `body` field.
+
+# !!!DANGER The `body` field is *cleared* when first accessed. This is a strange
+# quirk, but it do be like that. The safest way to handle the body (because we
+# do not want to lose our request!) is to store it in a variable.
+
+body = res.body
+
+# This is unexpected! We were promised an `application/json` content, and here
+# we are with a long array of unsigned 8-bit encoded integers. Why? In a
+# nutshell: there is no reason to expect that we will be querying text. We can
+# use *HTTP* to request sound, images, videos, or even streaming data. And so
+# what we get is the *raw* output. Thankfully, we can transform it into a
+# string:
+
+String(copy(body))
+
+# !!!WARNING We are using `copy` here because if we access `body` directly, it
+# *will* be cleared. The recommended design pattern when dealing with *HTTP*
+# responses it to process the `body` field in one go, to avoid losing this
+# information:
+
+# We are now a step away from having our JSON object:
+
+riki = JSON.parse(String(body))
+
+# !!!WARNING If you run this line a second time, it will fail -- this is because
+# you have access these `body` already, and so it is now empty. This module has
+# a lot of warnings. Welcome to working with remote data.
+
+# The output we get is now our standard JSON object, so we can do a little thing
+# like:
+
+for place in riki["places"]
+    @info "The post code for $(place["place name"]) is $(place["post code"])"
+end
+
+# Most APIs we use in practice for research are a lot more data-rich, and can
+# have highly structured fields.

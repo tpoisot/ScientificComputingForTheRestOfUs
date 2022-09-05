142dbd64b90a0445bd0cbafb66e3be5a6ef9e6e1
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-05 12:11:55 -0400

    💁 links to the packages in place

--- content/06_files/02_downloading.jl
+++ content/06_files/02_downloading.jl
@@ -2,3 +2,14 @@
 # title: Downloading files
 # status: alpha
 # ---
+
+# In this module, we will see how *Julia* allows downloading files from the
+# internet, and how we can decide where to store them. This is a common task
+# when getting external data, and will be the basis of a number of advanced
+# training modules in the final section of this material.
+
+# <!--more-->
+
+# The {{Downloads}} package is part of the standard library.
+
+using Downloads
--- content/06_files/03_delimited_files.jl
+++ content/06_files/03_delimited_files.jl
@@ -2,3 +2,5 @@
 # title: Working with delimited files
 # status: alpha
 # ---
+
+

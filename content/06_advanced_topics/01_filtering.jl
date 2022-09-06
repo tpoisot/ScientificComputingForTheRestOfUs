ec6f48ccc08425d1b65030abd874ffe27006089a
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-05 21:24:32 -0400

    🧑‍🎓 map/reduce

--- content/06_advanced_topics/01_filtering.jl
+++ content/06_advanced_topics/01_filtering.jl
@@ -21,3 +21,32 @@
 # \frac{x_1}{n} + \frac{x_2}{n} + ... + \frac{x_n}{n}
 # $$
 
+# In other words, for every element $x_i$, we want to divide it by $n$, and then
+# we want to reduce all of these using the sum operation. This is a task for
+# `map` and `reduce`.
+
+# The `map` function will take a function as argument, and apply it to every
+# element of a collection. For example, this is a way to use `map` to double
+# every entry in an array:
+
+x = [1, 2, 3, 4]
+map(e -> 2e, x)
+
+# On the other hand, `reduce` accepts a binary operator (something requiring two
+# arguments), and applies it sequentially alongside an array until a single
+# value remains. For example, the sum of an array expressed as `reduce` is:
+
+y = [1, 2, 4, 5]
+reduce(+, y)
+
+# And so, we have enough to build a very crude function to get the average of an
+# array of values:
+
+x = [1,2,3,4,5,6,7]
+n = length(x)
+reduce(+, map(e -> e/n, x))
+
+# !!!WARNING The `reduce` operation has no well defined behavior when using an
+# operator without associativity, like a substraction. This is because
+# `reduce(-, [a,b,c])` can be `a-(b-c)` or `(a-b)-c`; the documentation for
+# `reduce` has a number of alternatives to suggest.

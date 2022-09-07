7cf20e96ae78c1b2c710284f2c3f401d7f8d3a4e
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-09-07 15:18:55 -0400

    Ternary operator (#84)
    * 🧑‍🎓 ternary
    
    * 🧑‍🎓 update the ternary op module

--- content/01_fundamentals/05_ternary.jl
+++ content/01_fundamentals/05_ternary.jl
@@ -1,14 +1,17 @@
 # ---
 # title: The ternary operator
-# status: alpha
+# status: beta
 # ---
 
-# In this module
+# In this module, we will look at the "ternary operator", a very efficient
+# shortcut to perform a logical test in a single line. This is a construct we
+# will use quite a lot to express a conditional expression in a single line.
 
 # <!--more-->
 
 # Based on what we have seen in the previous modules, the way to store different
-# values in a variable would be to write something like
+# values in a variable according to some condition we set would be to write
+# something like:
 
 x::Float64 = 0.0
 if rand() < 0.5
@@ -23,17 +26,41 @@ end
 
 x = rand() < 0.5 ? 0.25 : 0.75
 
-# This is call a ternary operator. The basic syntax is `condition ? if true : if
-# false`. A single line, and we can handle both cases. This is obviously very
-# efficient to set the value of a variable depending on a condition.
+# This is called a ternary operator. The basic syntax is `condition ? if true :
+# if false`. It fits in a single line, and we can handle both cases. Note that
+# the cases are *returned* as a function of whether the condition is satisfied,
+# which is a way to rapidly fill a variable.
 
 # Another source of efficiency is that both sides of the `:` are *not*
 # evaluated, unlike other languages:
 
 true ? print(2) : print(3)
 
-# !!!INFO In fact, using the `Meta.@lower` macro, we can see that this is
-# converted to a `goto` statement, but this is a little beyond the scope of an
-# introduction module.
+# We can check this using the `@lower` macro from {{Meta}}: it is translating
+# *Julia* code into something of a lower-level, and is an interesting
+# opportunity to check what is going on "under the hood":
 
-# TODO using Bool as index this way
+Meta.@lower true ? cos(4) : sin(3)
+
+# We see in the output above that the *operations* (like `cos(4)`) have *not*
+# been expanded yet -- the ternary operator is "pointing" *Julia* towards the
+# right branch.
+
+# We can use the ternary operator as the most basic ingredient in a very naive
+# function that reproduces the Kronecker $\delta$ function: it returns 1 if the
+# two inputs are equal, and 0 if they are not. This function is generally
+# applied to non-negative integers.
+
+# !!!INFO Of course, if we wanted to do this properly, we could remember that
+# `false` *is* 0 and `true` *is* 1, and our function is not necessary.
+# Nevertheless, this is an interesting example to write from scratch:
+
+function δ(i::T, j::T) where {T <: Integer}
+    return i == j ? one(T) : zero(T)
+end
+
+# We can see what our function would do when applied to actual numbers:
+
+for i in 1:3, j in 1:3
+    @info "δ($(i), $(j)) = $(δ(i, j))"
+end

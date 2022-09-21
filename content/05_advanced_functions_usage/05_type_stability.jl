afa705fecf9bfa489d8b726876b339d632a79b3a
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-20 21:26:21 -0400

    🧑‍🎓 example type stability

--- content/05_advanced_functions_usage/05_type_stability.jl
+++ content/05_advanced_functions_usage/05_type_stability.jl
@@ -2,3 +2,16 @@
 # title: Type stability
 # status: alpha
 # ---
+
+# In this module, we will talk about *type stability*, and see how we can
+# annotate the functions in *Julia* to be explicit about what type they return.
+
+# <!--more-->
+
+function nope()::Nothing
+    return nothing
+end
+
+#-
+
+nope()

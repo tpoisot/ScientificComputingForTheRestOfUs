7fd7621691e7454143401ab37a91e4351ceee905
Timothée Poisot <timothee.poisot@umontreal.ca>
2022-09-24 21:52:16 -0400

    🐛 hotfix builder (#97)
    * 🐛 hotfix builder
    
    * 🐛 fix regexp to have new literate format
    
    * ⬆️ update deps and add a compat block in full

--- builder.jl
+++ builder.jl
@@ -2,20 +2,23 @@ using Literate
 import HTTP
 import Pkg
 
-# References
+# This code is here to manage the references -- references are specific callout
+# blocks that only have a bibtex key in them (for now), and can only be articles
+# (for now)
 include(joinpath(@__DIR__, "bibliorender.jl"))
 bibfile = joinpath(@__DIR__, "references.bib")
 references = Bibliography.import_bibtex(bibfile)
 
-# aiiiii
-files_to_build = []
+# This will store all of the .jl files that need to be built
+files_to_build = String[]
 
 # Get the top-level folders
 content_path = joinpath(@__DIR__, "content")
-
 mkpath(joinpath(@__DIR__, "dist", "content"))
 
-# Walk the content directory
+# Walk the content directory to identify the .jl files - everything that is in
+# the content folder and is a Julia file gets built, no exception. Note that
+# even the index pages are julia files
 for (root, dirs, files) in walkdir(content_path)
     source_files = filter(endswith(".jl"), files)
     if ~isempty(source_files)
@@ -23,7 +26,9 @@ for (root, dirs, files) in walkdir(content_path)
     end
 end
 
-# Function for callouts
+# This function feans with the five callouts: info, opinion, warning, danger,
+# and domain-specific info -- these are then handled by a Hugo shortcode + sass
+# code
 function replace_callouts(content)
     content = replace(
         content,
@@ -53,10 +58,15 @@ function replace_callouts(content)
     return content
 end
 
+# This function is a bit fucky because the plots all get sent to a specific
+# folder in the static files folder, so we need to look at every image link and
+# correct it directly in the markdown file. Also Literate is really fond of
+# changing the naming of images and so this wrecks out shit everytime. Thanks
+# Literate.
 function replace_images(content)
     content = replace(
         content,
-        r"!\[\]\((\d+.\w{3})\)\n" => s"\n![](/plots/\1)\n",
+        r"!\[\]\((.+.\w{3})\)\n" => s"\n![](/plots/\1)\n",
     )
     return content
 end
@@ -70,6 +80,8 @@ function replace_mermaid(content)
     return content
 end
 
+# This manages reference callouts. I forgot how. Some regexp and my apparently
+# net positive karma balance.
 function replace_reference(content)
     rx = r"!{3}[ ]{0,1}REF (.+)"
     content = replace(
@@ -82,6 +94,8 @@ function replace_reference(content)
     return content
 end
 
+# This one I like - it looks for a package name, and tries to look for its page
+# on the juliahub. If it doesn't exist, it's styled differently in the text.
 function fmt_pkg(pkgname)
     hub_url = "https://juliapackages.com/p/$(pkgname)"
     try
@@ -116,22 +130,37 @@ for file in files_to_build
     try
         destination_folder = replace(file, "content" => joinpath("dist", "content"))
         path_elements = splitpath(destination_folder)
-        # Folder and file
+        
+        # Get the correct paths
         root = joinpath(path_elements[1:(end - 1)])
         stem = replace(path_elements[end], ".jl" => "")
-        # Create the location
+        
+        # Create the location of the built lesson if it doesn't exist
         ispath(root) || mkpath(root)
-        Literate.markdown(
-            file,
-            root;
-            flavor = Literate.CommonMarkFlavor(),
-            config = Dict("credit" => false, "execute" => true),
-            name = stem,
-            postprocess = post_processor,
-        )
-        # Move images
-        images = filter(endswith(".png"), readdir(root; join = true))
-        append!(images, filter(endswith(".svg"), readdir(root; join = true)))
+        
+        # For local builds speedup, check file existence and mtime before
+        # deciding to build
+        dist = joinpath(root, stem*".md")
+        build = ~isfile(dist)
+        if isfile(dist)
+            build = mtime(file) > mtime(dist)
+        end
+
+        # Build the document
+        if build
+            Literate.markdown(
+                file,
+                root;
+                flavor = Literate.CommonMarkFlavor(),
+                config = Dict("credit" => false, "execute" => true),
+                name = stem,
+                postprocess = post_processor,
+            )
+        end
+
+        # Move images for this lesson
+        isimage(f) = endswith(".png")(f) || endswith(".svg")(f) 
+        images = filter(isimage, readdir(root; join = true))
         if ~isempty(images)
             images_go_to = joinpath(@__DIR__, "dist", "static", "plots")
             ispath(images_go_to) || mkpath(images_go_to)

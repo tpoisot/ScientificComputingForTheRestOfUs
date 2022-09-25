296a8021c8389d0d0a0b34d97eb6650faeabcbba
Timothée Poisot (he/him) <timothee.poisot@umontreal.ca>
2022-09-25 16:41:25 -0400

    OVERLOADING

--- content/06_advanced_topics/03_overloading.jl
+++ content/06_advanced_topics/03_overloading.jl
@@ -1,8 +1,11 @@
 # ---
 # title: Overloading
-# status: alpha
+# status: beta
 # ---
 
-# Intro
+# Overloading is a *very* powerful mechanism, through which we can add methods
+# to existing functions to make them work with our own types. In this module, we
+# will discuss how to overload existing functions, and how to use this approach
+# in practice.
 
 # <!--more-->
